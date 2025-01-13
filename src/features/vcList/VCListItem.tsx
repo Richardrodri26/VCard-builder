bacde07a97bcb2a2ce64c1a7418bb957badbb45a
RichardRodri26 <richardmanuel811@gmail.com>
2025-01-12 20:21:42 -0500

    Add navigation to VCListItem for viewing specific VCard details

--- src/features/vcList/VCListItem.tsx
+++ src/features/vcList/VCListItem.tsx
@@ -1,9 +1,15 @@
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import { Search } from 'lucide-react';
+import { useNavigate } from 'react-router-dom';
 
 export const VCListItem = () => {
-  
+  const navigate = useNavigate();
+
+  const goToViewVCard = () => {
+    navigate('/vcard/1')
+  }
+
   return (
     <Card className='flex-1'>
       <CardHeader>
@@ -11,7 +17,7 @@ export const VCListItem = () => {
         <CardDescription>Descripción</CardDescription>
       </CardHeader>
       <CardContent>
-        <Button variant="outline">
+        <Button onClick={goToViewVCard} variant="outline">
           <Search />
           <span>Ver</span>
         </Button>
